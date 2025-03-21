bba8f2282d128d93de00d5136518886acfd6a4e9
link2sshoaibansari <link2shoaibansari@gmail.com>
2025-03-22 00:36:38 +0530

    clickable button making

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient:LinearGradient(
+              colors: [
+                Color.fromARGB(255, 55, 21, 113),
+                Color.fromARGB(255, 143, 108, 203),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              ),
+          ),
+          child: StartScreen()
+          ),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,30 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+          width: 300),
+          const SizedBox(height: 20),
+          Text('Learn flutter the fun way!',
+          style: TextStyle(
+            color: const Color.fromARGB(255, 26, 19, 56),
+            fontSize: 20,
+          ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: (){},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: 
+              const Color.fromARGB(255, 176, 159, 223)
+            ),
+            child: const Text('Start Quiz'),
+            )
+        ],
+      ),
+      );
   }
 }
