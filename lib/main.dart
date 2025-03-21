87f962c15a31d8d068052d1dfb1d927cadad0ef7
link2sshoaibansari <link2shoaibansari@gmail.com>
2025-03-21 23:52:00 +0530

    clone

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_box.dart';
+
+void main() {
+  runApp(MaterialApp(home: 
+  Scaffold(body: 
+  GradientBox(),
+  ),
+  ),
+  );
+}
