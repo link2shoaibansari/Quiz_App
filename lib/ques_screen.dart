b1fdc1b2a94ebf3f47988a2bb1ca6d9ea2ce83d8
link2sshoaibansari <link2shoaibansari@gmail.com>
2025-04-10 01:07:36 +0530

    link to next questions
    task:
    
       when user selects any options , then it goes to the next question automatically.

--- lib/ques_screen.dart
+++ lib/ques_screen.dart
@@ -11,9 +11,18 @@ class QuesScreen extends StatefulWidget {
 }
 
 class _QuesContent extends State<QuesScreen> {
+
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    // to change question index at each click
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex]; //calling current question index
 
     // return const Text("Questions are Here!!");
     // now we want to display qustion and below that their answers:
@@ -23,36 +32,34 @@ class _QuesContent extends State<QuesScreen> {
       child: Container(
         margin: const EdgeInsets.all(40), //spacig between options buttons
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,   // vertical axis
-          crossAxisAlignment: CrossAxisAlignment.stretch,// horizontal axis
+          mainAxisAlignment: MainAxisAlignment.center, // vertical axis
+          crossAxisAlignment: CrossAxisAlignment.stretch, // horizontal axis
           children: [
             Text(
               // 'The question..',
-              currentQuestion.text,  // calling question list index at [0]
-              style: const TextStyle(
-                color: Colors.white),
-                textAlign: TextAlign.center, // text in centre of the screen
+              currentQuestion.text, // calling question list index at [0]
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center, // text in centre of the screen
             ),
-            SizedBox(height: 30), 
-            
+            SizedBox(height: 30),
+
             // spreading values(...)
             // ...currentQuestion.answers.map((answer) {
 
             // calling getShuffledAnswers for shuffled options
-
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
-            })
-        
+              return AnswerButton(answerText: answer, onTap: answerQuestion); 
+            }),    //calling answerQuestion function when clicking onto the button
+
             // using spreadin values(...), no need of defining
             // below code seperately
-        
+
             // accessing answers list index wise:
-            //although writing code repeatedly is not recommended 
-          //   AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
-          //   AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
-          //   AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
-          //   AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+            //although writing code repeatedly is not recommended
+            //   AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+            //   AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+            //   AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+            //   AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
           ],
         ),
       ),
