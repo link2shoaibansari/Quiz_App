885b3e2e6d1ed6f77abf19073c5ead2f894cb446
link2sshoaibansari <link2shoaibansari@gmail.com>
2025-04-10 00:58:05 +0530

    shuffle answers, give padding, and styling
    tasks:
    1. provide padding to the options.
    2. shuffle the options(as the first
        option is the correct answer)

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,8 +1,13 @@
 class QuizQuestion {
-
   QuizQuestion(this.text, this.answers);
 
-  final String text; 
+  final String text;
   final List<String> answers;
 
+  //method for shuffling the asnwers
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers); // create a copy of original answers list
+    shuffledList.shuffle();          // then it shuffles the asnwers
+    return shuffledList;             // return the updated shuffled list
+  }
 }
--- lib/ques_screen.dart
+++ lib/ques_screen.dart
@@ -20,32 +20,41 @@ class _QuesContent extends State<QuesScreen> {
     return SizedBox(
       // to display options in whole screen
       width: double.infinity, // as wide as possible acc to the screen
-      child: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.center, //content at center of screen
-        children: [
-          Text(
-            // 'The question..',
-            currentQuestion.text,  // calling question list index at [0]
-            style: const TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 30), 
-          
-          // spreading values(...)
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          })
+      child: Container(
+        margin: const EdgeInsets.all(40), //spacig between options buttons
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,   // vertical axis
+          crossAxisAlignment: CrossAxisAlignment.stretch,// horizontal axis
+          children: [
+            Text(
+              // 'The question..',
+              currentQuestion.text,  // calling question list index at [0]
+              style: const TextStyle(
+                color: Colors.white),
+                textAlign: TextAlign.center, // text in centre of the screen
+            ),
+            SizedBox(height: 30), 
+            
+            // spreading values(...)
+            // ...currentQuestion.answers.map((answer) {
 
-          // using spreadin values(...), no need of defining
-          // below code seperately
+            // calling getShuffledAnswers for shuffled options
 
-          // accessing answers list index wise:
-          //although writing code repeatedly is not recommended 
-        //   AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
-        //   AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
-        //   AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
-        //   AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
-        ],
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+        
+            // using spreadin values(...), no need of defining
+            // below code seperately
+        
+            // accessing answers list index wise:
+            //although writing code repeatedly is not recommended 
+          //   AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          //   AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          //   AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          //   AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+          ],
+        ),
       ),
     );
   }
