c393edcf6abcf59ffc08886034172abb85d44682
link2sshoaibansari <link2shoaibansari@gmail.com>
2025-04-09 23:27:54 +0530

    spreading values, and more ways to display questions and their options
    Using spreading values(...), no need of defining answer code separately.
    
    Syntax:
    ...currentQuestion.answers.map((answer)
     {
       return AnswerButton(
    answerText: answer,
     onTap: () {});

--- lib/ques_screen.dart
+++ lib/ques_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuesScreen extends StatefulWidget {
   const QuesScreen({super.key});
@@ -12,6 +13,8 @@ class QuesScreen extends StatefulWidget {
 class _QuesContent extends State<QuesScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     // return const Text("Questions are Here!!");
     // now we want to display qustion and below that their answers:
     return SizedBox(
@@ -21,18 +24,28 @@ class _QuesContent extends State<QuesScreen> {
         mainAxisAlignment:
             MainAxisAlignment.center, //content at center of screen
         children: [
-          Text('The question..', style: TextStyle(color: Colors.white)),
-          SizedBox(height: 30),
-          AnswerButton(
-            answerText: 'Answer 1', 
-            onTap: () {}),
-          AnswerButton(
-            answerText: 'Answer 2', 
-            onTap: () {}),
-          AnswerButton(
-            answerText: 'Answer 3', 
-            onTap: () {}),
-          ],
+          Text(
+            // 'The question..',
+            currentQuestion.text,  // calling question list index at [0]
+            style: const TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 30), 
+          
+          // spreading values(...)
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          })
+
+          // using spreadin values(...), no need of defining
+          // below code seperately
+
+          // accessing answers list index wise:
+          //although writing code repeatedly is not recommended 
+        //   AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+        //   AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+        //   AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+        //   AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+        ],
       ),
     );
   }
